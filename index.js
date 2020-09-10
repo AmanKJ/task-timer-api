3ac2635a72c1299366c2c4afc4ebf4da20ebca38
Aman Kumar <mraman1997@gmail.com>
2020-09-10 15:29:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const { PORT } = require("./config/keys");
 require("./loaders/db")();
 require("./loaders/routes")(app);
 
-const port = app.listen(port, () => console.log(`Server listening at port:${port}`));
+app.listen(PORT, () => console.log(`Server listening at port:${PORT}`));
